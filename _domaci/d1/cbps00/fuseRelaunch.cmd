1320bf02b3ffa4d03f7d0b5875217007524bfde6
l4v <ivos.jovan@gmail.com>
2018-10-29 09:33:43 +0100

    Your branch is up to date with 'origin/master'. DOMACI FINISHED

--- _domaci/d1/cbps00/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "/home/hk-47/workspace/xilinx/_domaci/d1/cbps00/cbps00_cbps00_sch_tb_isim_beh.exe" -prj "/home/hk-47/workspace/xilinx/_domaci/d1/cbps00/cbps00_cbps00_sch_tb_beh.prj" "work.cbps00_cbps00_sch_tb" 
